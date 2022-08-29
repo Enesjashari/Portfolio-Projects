85296e955fdf9f93dc86f25c7f13c758d9277d08
Enesjashari <enesjashari2004@gmail.com>
2022-08-28 22:20:39 -1100

    Nes Magazine
    Nes Magazine Project made using Django added

---
+++ Nes Magazine/posts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Tech)
+admin.site.register(HeaderPost)
+admin.site.register(Showbiz)
+admin.site.register(Travel)
+admin.site.register(Sport)
+
+
+
+#   createsuperuser credencials
+
+#   username = e
+#   password = e
