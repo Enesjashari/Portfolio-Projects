416519446affe881191bdb00c6b46d5849cf5fc0
Enesjashari <enesjashari2004@gmail.com>
2022-09-07 10:55:14 -1100

    Sky Commerce created
    Also Calculator Updated

---
+++ Sky Commerce/README.md
@@ -0,0 +1,2 @@
+# Sky Commerce e-commerce
+ Personal projects(web-apps) made using Django & Python .
---
+++ Sky Commerce/app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+'''
+superuser username & password
+username : e
+password : e
+'''
